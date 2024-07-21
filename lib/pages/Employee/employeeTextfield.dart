675e0c596fa0a67c185089e8e20b9a21dc281c4b
Amrut-03 <amrut@gmail.com>
2024-07-21 18:11:59 +0530

    some changes are done

--- lib/pages/Employee/employeeTextfield.dart
+++ lib/pages/Employee/employeeTextfield.dart
@@ -1,25 +1,116 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:car_wash/Widgets/ButtonWidget.dart';
 import 'package:car_wash/Widgets/TextFieldWidget.dart';
+import 'package:car_wash/provider/provider.dart';
 import 'package:car_wash/utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 
-class EmployeeTextfield extends StatefulWidget {
+class EmployeeTextfield extends ConsumerWidget {
   const EmployeeTextfield({super.key});
 
-  @override
-  State<EmployeeTextfield> createState() => _EmployeeTextfieldState();
-}
+  Future<void> _pickImage(BuildContext context, WidgetRef ref,
+      StateProvider<File?> imageProvider) async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.camera);
+    if (pickedFile != null) {
+      final File file = File(pickedFile.path);
+      ref.read(imageProvider.notifier).state = file;
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Image Uploaded Successfully")));
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("No image picked")));
+    }
+  }
 
-class _EmployeeTextfieldState extends State<EmployeeTextfield> {
-  TextEditingController employeeController = TextEditingController();
-  TextEditingController dobController = TextEditingController();
-  TextEditingController addressController = TextEditingController();
-  TextEditingController phone1Controller = TextEditingController();
-  TextEditingController phone2Controller = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  void createEmployee(BuildContext context, WidgetRef ref) async {
+    var request = http.MultipartRequest('POST',
+        Uri.parse('https://wash.sortbe.com/API/Admin/User/Employee-Creation'));
+    request.fields.addAll({
+      'enc_key': 'C0oRAe1QNtn3zYNvJ8rv',
+      'emp_id': '123',
+      'emp_name': ref.read(employeeControllerProvider).text,
+      'dob': ref.read(dobControllerProvider).text,
+      'address': ref.read(addressControllerProvider).text,
+      'phone_1': ref.read(phone1ControllerProvider).text,
+      'phone_2': ref.read(phone2ControllerProvider).text,
+      'password': ref.read(passwordControllerProvider).text,
+      'role': 'Employee'
+    });
+
+    final adharFront = ref.read(aadharFrontProvider.state).state;
+    final adharBack = ref.read(aadharBackProvider.state).state;
+    final driveFront = ref.read(driveFrontProvider.state).state;
+    final driveBack = ref.read(driveBackProvider.state).state;
+    final employeePhoto = ref.read(employeePhotoProvider.state).state;
+
+    if (adharFront != null) {
+      request.files.add(
+          await http.MultipartFile.fromPath('aadhaar_front', adharFront.path));
+    }
+    if (adharBack != null) {
+      request.files.add(
+          await http.MultipartFile.fromPath('aadhaar_back', adharBack.path));
+    }
+    if (driveFront != null) {
+      request.files.add(
+          await http.MultipartFile.fromPath('driving_front', driveFront.path));
+    }
+    if (driveBack != null) {
+      request.files.add(
+          await http.MultipartFile.fromPath('driving_back', driveBack.path));
+    }
+    if (employeePhoto != null) {
+      request.files.add(
+          await http.MultipartFile.fromPath('emp_photo', employeePhoto.path));
+    }
+
+    http.StreamedResponse response = await request.send();
+    String temp = await response.stream.bytesToString();
+    var body = jsonDecode(temp);
+
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(body['status'])));
+    } else {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(body['status'])));
+    }
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final adharFront = ref.watch(aadharFrontProvider);
+    final adharBack = ref.watch(aadharBackProvider);
+    final driveFront = ref.watch(driveFrontProvider);
+    final driveBack = ref.watch(driveBackProvider);
+    final employeePhoto = ref.watch(employeePhotoProvider);
+
+    Widget imagePreview(File? image) {
+      return image != null
+          ? ClipRRect(
+              borderRadius: BorderRadius.circular(5.r),
+              child: Image.file(
+                image,
+                height: 85.w,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            )
+          : Image.asset(
+              'assets/images/Camera.png',
+              height: 45.w,
+              width: double.infinity,
+            );
+    }
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -30,29 +121,25 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
             labelTxtClr: const Color(0xFF929292),
             enabledBorderClr: const Color(0xFFD4D4D4),
             focusedBorderClr: const Color(0xFFD4D4D4),
-            controller: employeeController,
+            controller: ref.read(employeeControllerProvider),
           ),
         ),
-        SizedBox(
-          height: 20.h,
-        ),
+        SizedBox(height: 20.h),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 25.w),
           child: Textfieldwidget(
-            controller: dobController,
+            controller: ref.read(dobControllerProvider),
             labelTxt: 'Date of Birth',
             labelTxtClr: const Color(0xFF929292),
             enabledBorderClr: const Color(0xFFD4D4D4),
             focusedBorderClr: const Color(0xFFD4D4D4),
           ),
         ),
-        SizedBox(
-          height: 20.h,
-        ),
+        SizedBox(height: 20.h),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 25.w),
           child: TextField(
-            controller: addressController,
+            controller: ref.read(addressControllerProvider),
             maxLines: 4,
             cursorColor: AppTemplate.enabledBorderClr,
             decoration: InputDecoration(
@@ -74,48 +161,40 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
             ),
           ),
         ),
-        SizedBox(
-          height: 20.h,
-        ),
+        SizedBox(height: 20.h),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 25.w),
           child: Textfieldwidget(
-            controller: phone1Controller,
+            controller: ref.read(phone1ControllerProvider),
             labelTxt: 'Phone 1',
             labelTxtClr: const Color(0xFF929292),
             enabledBorderClr: const Color(0xFFD4D4D4),
             focusedBorderClr: const Color(0xFFD4D4D4),
           ),
         ),
-        SizedBox(
-          height: 20.h,
-        ),
+        SizedBox(height: 20.h),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 25.w),
           child: Textfieldwidget(
-            controller: phone2Controller,
+            controller: ref.read(phone2ControllerProvider),
             labelTxt: 'Phone 2',
             labelTxtClr: const Color(0xFF929292),
             enabledBorderClr: const Color(0xFFD4D4D4),
             focusedBorderClr: const Color(0xFFD4D4D4),
           ),
         ),
-        SizedBox(
-          height: 20.h,
-        ),
+        SizedBox(height: 20.h),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 25.w),
           child: Textfieldwidget(
-            controller: passwordController,
+            controller: ref.read(passwordControllerProvider),
             labelTxt: 'Password',
             labelTxtClr: const Color(0xFF929292),
             enabledBorderClr: const Color(0xFFD4D4D4),
             focusedBorderClr: const Color(0xFFD4D4D4),
           ),
         ),
-        SizedBox(
-          height: 20.h,
-        ),
+        SizedBox(height: 20.h),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 25.w),
           child: Text(
@@ -127,15 +206,13 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
             ),
           ),
         ),
-        SizedBox(
-          height: 10.h,
-        ),
+        SizedBox(height: 10.h),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 25.w),
           child: Row(
             children: [
               GestureDetector(
-                onTap: () {},
+                onTap: () => _pickImage(context, ref, aadharFrontProvider),
                 child: Container(
                   width: 120.w,
                   height: 80.h,
@@ -156,22 +233,17 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(5.r),
-                        child: Image.asset(
-                          'assets/images/Camera.png',
-                          height: 45.w,
-                          width: double.infinity,
-                        ),
-                      ),
-                      Text(
-                        'Front Side',
-                        style: GoogleFonts.inter(
-                          fontSize: 12.sp,
-                          color: const Color(0xFF6750A4),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
+                      imagePreview(adharFront),
+                      adharFront == null
+                          ? Text(
+                              'Front Side',
+                              style: GoogleFonts.inter(
+                                fontSize: 12.sp,
+                                color: const Color(0xFF6750A4),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          : Container()
                     ],
                   ),
                 ),
@@ -180,7 +252,7 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
                 width: 20.w,
               ),
               GestureDetector(
-                onTap: () {},
+                onTap: () => _pickImage(context, ref, aadharBackProvider),
                 child: Container(
                   width: 120.w,
                   height: 80.h,
@@ -201,22 +273,17 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(5.r),
-                        child: Image.asset(
-                          'assets/images/Camera.png',
-                          height: 45.w,
-                          width: double.infinity,
-                        ),
-                      ),
-                      Text(
-                        'Back Side',
-                        style: GoogleFonts.inter(
-                          fontSize: 12.sp,
-                          color: const Color(0xFF6750A4),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
+                      imagePreview(adharBack),
+                      adharBack == null
+                          ? Text(
+                              'Back Side',
+                              style: GoogleFonts.inter(
+                                fontSize: 12.sp,
+                                color: const Color(0xFF6750A4),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          : Container()
                     ],
                   ),
                 ),
@@ -247,7 +314,7 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
             // mainAxisAlignment: MainAxisAlignment.center,
             children: [
               GestureDetector(
-                onTap: () {},
+                onTap: () => _pickImage(context, ref, driveFrontProvider),
                 child: Container(
                   width: 120.w,
                   height: 80.h,
@@ -268,22 +335,17 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(5.r),
-                        child: Image.asset(
-                          'assets/images/Camera.png',
-                          height: 45.w,
-                          width: double.infinity,
-                        ),
-                      ),
-                      Text(
-                        'Front Side',
-                        style: GoogleFonts.inter(
-                          fontSize: 12.sp,
-                          color: const Color(0xFF6750A4),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
+                      imagePreview(driveFront),
+                      driveFront == null
+                          ? Text(
+                              'Front Side',
+                              style: GoogleFonts.inter(
+                                fontSize: 12.sp,
+                                color: const Color(0xFF6750A4),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          : Container()
                     ],
                   ),
                 ),
@@ -292,7 +354,7 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
                 width: 20.w,
               ),
               GestureDetector(
-                onTap: () {},
+                onTap: () => _pickImage(context, ref, driveBackProvider),
                 child: Container(
                   width: 120.w,
                   height: 80.h,
@@ -313,22 +375,17 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(5.r),
-                        child: Image.asset(
-                          'assets/images/Camera.png',
-                          height: 45.w,
-                          width: double.infinity,
-                        ),
-                      ),
-                      Text(
-                        'Back Side',
-                        style: GoogleFonts.inter(
-                          fontSize: 12.sp,
-                          color: const Color(0xFF6750A4),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
+                      imagePreview(driveBack),
+                      driveBack == null
+                          ? Text(
+                              'Back Side',
+                              style: GoogleFonts.inter(
+                                fontSize: 12.sp,
+                                color: const Color(0xFF6750A4),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          : Container()
                     ],
                   ),
                 ),
@@ -359,7 +416,7 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
             // mainAxisAlignment: MainAxisAlignment.center,
             children: [
               GestureDetector(
-                onTap: () {},
+                onTap: () => _pickImage(context, ref, employeePhotoProvider),
                 child: Container(
                   width: 120.w,
                   height: 80.h,
@@ -380,22 +437,17 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(5.r),
-                        child: Image.asset(
-                          'assets/images/Camera.png',
-                          height: 45.w,
-                          width: double.infinity,
-                        ),
-                      ),
-                      Text(
-                        'Upload Photo',
-                        style: GoogleFonts.inter(
-                          fontSize: 12.sp,
-                          color: const Color(0xFF6750A4),
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
+                      imagePreview(employeePhoto),
+                      employeePhoto == null
+                          ? Text(
+                              'Upload Photo',
+                              style: GoogleFonts.inter(
+                                fontSize: 12.sp,
+                                color: const Color(0xFF6750A4),
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          : Container()
                     ],
                   ),
                 ),
@@ -403,6 +455,26 @@ class _EmployeeTextfieldState extends State<EmployeeTextfield> {
             ],
           ),
         ),
+        SizedBox(height: 20.h),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Buttonwidget(
+              width: 227.w,
+              height: 50.h,
+              buttonClr: const Color(0xFf1E3763),
+              txt: 'Create',
+              textClr: AppTemplate.primaryClr,
+              textSz: 18.sp,
+              onClick: () {
+                createEmployee(context, ref);
+              },
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20.h,
+        )
       ],
     );
   }
